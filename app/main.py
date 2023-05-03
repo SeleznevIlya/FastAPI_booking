cb5f2635bba25f9b0acc502f640c82f35ef6df9d
Илья Селезнев <ilya_seleznev@MacBook-Air-Ila.local>
2023-05-03 21:50:17 +0300

    add CORS

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
+from starlette.middleware.cors import CORSMiddleware
 
 from app.bookings.router import router as router_bookings
 from app.auth.router import router as auth_router
@@ -11,8 +12,23 @@ app = FastAPI()
 
 app.mount("/static", StaticFiles(directory="app/static"), "static")
 
+origins = [
+    "http://localhost",
+    "http://localhost:8080",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["GET", "POST", "OPTIONS", "DELETE", "PATCH", "PUT"],
+    allow_headers=["Content-Type", "Set-Cookie", "Access-Control-Allow-Headers", "Access-Authorization"],
+)
+
 app.include_router(auth_router)
 app.include_router(router_bookings)
 app.include_router(router_hotels)
 app.include_router(router_pages)
 app.include_router(router_images)
+
+
