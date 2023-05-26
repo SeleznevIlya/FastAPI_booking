9792547e72072400f68fbffe5807ff6b21852cf1
Илья Селезнев <ilya_seleznev@MacBook-Air-Ila.local>
2023-05-26 23:32:50 +0300

    create 'upload' for create db_objects from csv file

--- app/importer/router.py
+++ app/importer/router.py
@@ -1,8 +1,12 @@
 from fastapi import APIRouter, UploadFile, File
 import csv
 import codecs
-
+from sqlalchemy import insert
 from starlette.background import BackgroundTasks
+from app.hotels.schemas import SHotel
+from app.hotels.models import Hotel
+
+from app.database import async_session_maker
 
 router = APIRouter(
 	prefix="/importer",
@@ -11,9 +15,20 @@ router = APIRouter(
 
 
 @router.post("/upload")
-def upload(background_tasks: BackgroundTasks, file: UploadFile = File(...)):
-	csvReader = csv.DictReader(codecs.iterdecode(file.file, 'utf-8'))
+async def upload(background_tasks: BackgroundTasks, file: UploadFile = File(...)):
+	csv_reader = csv.DictReader(codecs.iterdecode(file.file, 'utf-8'))
 	background_tasks.add_task(file.file.close)
-	return list(csvReader)
+	for hotel in list(csv_reader):
+		for key in hotel:
+			try:
+				hotel[key] = int(hotel[key])
+			except:
+				continue
+		async with async_session_maker() as session:
+			query = insert(Hotel).values(**hotel)
+			await session.execute(query)
+			await session.commit()
+	return None
+
 
 
